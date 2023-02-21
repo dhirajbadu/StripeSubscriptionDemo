7ccb9a6e8e10f28b9ea967fb105c2461d8d2a5fc
PritamThing <pritamthing90@gmail.com>
2023-02-21 12:35:03 +0545

    unhandled events

--- grails-app/services/stripedemo/StripeService.groovy
+++ grails-app/services/stripedemo/StripeService.groovy
@@ -393,7 +393,7 @@ class StripeService {
         if (!SubscriptionPlan.findByTitleInList([SubscriptionType.BASIC, SubscriptionType.PREMIUM])) {
             SubscriptionPlan.saveAll(List.of(
                     new SubscriptionPlan(SubscriptionType.FREE, SubscriptionPeriod.FREE, 0L),
-                    new SubscriptionPlan(SubscriptionType.BASIC, SubscriptionPeriod.MONTHLY, 9L),
+                    new SubscriptionPlan(SubscriptionType.BASIC, SubscriptionPeriod.MONTHLY, 10L),
                     new SubscriptionPlan(SubscriptionType.PREMIUM, SubscriptionPeriod.MONTHLY, 20L),
                     new SubscriptionPlan(SubscriptionType.BASIC, SubscriptionPeriod.YEARLY, 100L),
                     new SubscriptionPlan(SubscriptionType.PREMIUM, SubscriptionPeriod.YEARLY, 300L)
